a9e232319500e6506f4f464ca38179bec3dfea7e
kuhakuu04 <mhudafiqri06@gmail.com>
2021-02-21 22:09:03 +0800

    Menambahkan Materi Baru

---
+++ Layout/Horizontal Layout/main.py
@@ -0,0 +1,35 @@
+'''
+menentukan tampilan dengan horizontal layout
+
+sumber referensi: https://pythonpyqt.com/pyqt-box-layout/
+ditulis pada: 21-02-2021
+'''
+
+from PyQt5.QtWidgets import *
+
+class MainApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.MainUI()
+        self.setGeometry(10, 10, 200, 150)
+        self.setWindowTitle('Frame Horizontal')
+
+    def MainUI(self):
+
+        btn1 = QPushButton('Click Me!!! Button 1')
+        btn1.setGeometry(10, 10, 0, 30)
+
+        btn2 = QPushButton('Click Me!!! Button 2')
+        btn1.setGeometry(10, 10, 0, 30)
+
+        herlay = QHBoxLayout(self)
+        herlay.addWidget(btn1)
+        herlay.addWidget(btn2)
+
+app = QApplication([])
+
+module = MainApp()
+module.show()
+
+app.exec()
